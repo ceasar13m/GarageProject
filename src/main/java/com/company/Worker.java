b8a6ea38d57070ba8928436560c721b56d87c65a
1502-Ilnur <muharlyamovy@gmail.com>
2019-02-16 09:16:28 +0300

    f1

--- src/main/java/com/company/Worker.java
+++ src/main/java/com/company/Worker.java
@@ -1,31 +1,42 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Worker {
 
+    final int commandNumbForParsing = 1;
+    final  int jsonNumbForParsing = 2;
+
     public Worker(String message) {
-        sorter(message);
+        sorter(parseCommand(message, commandNumbForParsing));
     }
 
     private void sorter(String message) {
+        if(message != null)
         switch (message) {
             case "signup": {
                 System.out.println("SIGNUP");
+                processSignUp();
                 break;
             }
             case "signin": {
                 System.out.println("SIGNIN");
+                processSignIn();
                 break;
             }
             case "add": {
                 System.out.println("ADD");
+                processAdd();
                 break;
             }
             case "buy": {
                 System.out.println("BUY");
+                processBuy();
                 break;
             }
             case "getall": {
                 System.out.println("GETALL");
+                processGetAll();
                 break;
             }
             default: {
@@ -33,5 +44,39 @@ public class Worker {
             }
         }
     }
+
+    private String parseCommand(String url, int i) {
+        String[] split = url.split("/");
+
+        if (split.length < i + 1) return null;
+        return split[i];
+
+    }
+
+    private void processSignIn(String url) {
+
+    }
+
+    private void processSignUp(String url) {
+
+    }
+
+    private void processAdd() throws IOException {
+
+    }
+
+    private void processBuy(String url) {
+
+    }
+
+    private void processGetAll(String url) {
+
+    }
+
+    private void processSignOut(String url) {
+
+    }
+
+
 
 }
